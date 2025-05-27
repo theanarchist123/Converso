fef7f2c523ac26340641177b104ddf182e64e069
theanarchist123 <2023.nikhil.kadam@ves.ac.in>
2025-05-27 15:45:45 +0530

    Clerk billing one successfully...

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import {PricingTable} from "@clerk/nextjs";
 
 
 const Subscription = () => {
     return (
         <div>
-            Subscription
+            <PricingTable/>
         </div>
     )
 }
